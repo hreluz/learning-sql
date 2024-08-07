fb73646f5f34413698b60cd050db1ac04fbdc4bd
Hector <hmreluz@gmail.com>
2024-08-07 11:01:53 -0500

    academind: sql-complete-guide: 1-data-definition: insert data

---
+++ academind/1.sql-the-complete-guide/1-data-definition/03-insert-users-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (full_name, yearly_salary, current_status) VALUES ('James Bond', 25000, 'self-employed');
+INSERT INTO users (full_name, yearly_salary, current_status) VALUES ('Peter Parker', 0, 'unemployed');
+INSERT INTO users (full_name, yearly_salary, current_status) VALUES ('Bruce Wayne', 35000, 'employed');
