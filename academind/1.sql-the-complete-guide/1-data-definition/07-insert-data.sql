58de01118680d967c99b63284f9533a38cf0e86e
Hector <hmreluz@gmail.com>
2024-08-07 11:53:21 -0500

    academind: sql-complete-guide: 1-data-definition: insert data on tables

---
+++ academind/1.sql-the-complete-guide/1-data-definition/07-insert-data.sql
@@ -0,0 +1,12 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES ('Learning Inc', 'Evergreen 555, Springfield', 0.87, TRUE );
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES ('Big Oil Inc', 'Slipperystreet 100, Houston', 112.55, FALSE );
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES ('Hisper Food', 'Avocado Street5, Berlin', 6.12, TRUE );
+
+
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+VALUES ('James Bond', 'Learning Inc', 'Hi there', '2024-07-08 20:20:20');
