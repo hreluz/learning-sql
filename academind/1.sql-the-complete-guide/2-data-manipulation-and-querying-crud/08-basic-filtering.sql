f4e3ebc2ed9f665559d09e7a03d0cb236532a9b4
Hector <hmreluz@gmail.com>
2024-08-07 17:55:11 -0500

    academind: sql-complete-guide: 2-data-manipulation: filtering: basic, date and text

---
+++ academind/1.sql-the-complete-guide/2-data-manipulation-and-querying-crud/08-basic-filtering.sql
@@ -0,0 +1,16 @@
+
+-- 1 - Get all volumes higher> than 1000
+-- SELECT * 
+-- FROM sales
+-- WHERE volume > 1000;
+
+-- 4 - Find all recurring sales 
+-- SELECT * 
+-- FROM sales
+-- WHERE is_recurring = TRUE;
+
+-- 5 - Find all the disputed with volumes higher than 5000
+-- SELECT *
+-- FROM sales
+-- WHERE (is_disputed IS TRUE)
+-- AND (volume > 5000);
