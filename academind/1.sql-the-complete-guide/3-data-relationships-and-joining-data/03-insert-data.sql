9f40cf5a204827742fdc24fb136c2056dd129861
Hector <hmreluz@gmail.com>
2024-08-07 18:57:57 -0500

    academind: sql-complete-guide: 3-data-relationships-and-joining-data: insert data

---
+++ academind/1.sql-the-complete-guide/3-data-relationships-and-joining-data/03-insert-data.sql
@@ -0,0 +1,18 @@
+-- Cities
+-- INSERT INTO cities (name) VALUES ('Berlin'),('Paris'), ('Rome'), ('New York'), ('London');
+
+
+-- Addresses
+-- INSERT INTO addresses (street, house_number, city_id) VALUES
+-- ('Test street', '8A', 1),
+-- ('Evergreen street', '8 F3', 2),
+-- ('Wall street', '213', 3),
+-- ('Green street', '999', 4);
+
+
+-- Users
+INSERT INTO users (first_name, last_name, email, address_id) VALUES
+('Bruce', 'Wayne', 'batman@gmail.com', 1),
+('Clark', 'Kent', 'superman@gmail.com', 2),
+('Diana', 'T', 'wonderwoman@gmail.com', 3),
+('Barry', 'Allen', 'flash@gmail.com', 4);
