0c54818ef8c49d501d9ee6d1de97916392b7a3ff
Hector <hmreluz@gmail.com>
2024-08-07 11:18:05 -0500

    academind: sql-complete-guide: 1-data-definition: create additional \tables

---
+++ academind/1.sql-the-complete-guide/1-data-definition/05-create-employers-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers(
+    company_name VARCHAR(250),
+    company_address VARCHAR(300),
+    -- yearly_revenue FLOAT(5,2) -- Approximaton: 123.13 12.1 , Not allowed: 1234.12 1.2354354
+    yearly_revenue NUMERIC(5,2), -- Exact Value, Alloed: 123.12, Not Allowed: 12345.43
+    is_hiring BOOLEAN
+);
