46ca6a304b7de2f132df8ac226f138680e7115ed
Hector <hmreluz@gmail.com>
2024-08-07 13:44:14 -0500

    academind: sql-complete-guide: 1-data-definition: auto incrementing ids and primary keys

---
+++ academind/1.sql-the-complete-guide/1-data-definition/17-primary-key.sql
@@ -0,0 +1,54 @@
+DROP TABLE users;
+DROP TABLE employers;
+DROP TABLE conversations;
+
+-- Postgresql
+-- CREATE TYPE employment_status AS  ENUM('employed', 'self-employed', 'unemployed');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY , -- POSTGRESQL
+    full_name VARCHAR(300) NOT NULL,
+    yearly_salary INT CHECK(yearly_salary > 0),
+    current_status employment_status -- PostgreSQL Syntax
+);
+
+CREATE TABLE employers(
+    id SERIAL PRIMARY KEY , -- POSTGRESQL
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue FLOAT CHECK (yearly_revenue > 0),
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE conversations (
+    id SERIAL PRIMARY KEY , -- POSTGRESQL
+    user_name VARCHAR(200),
+    employer_name VARCHAR(250),
+    message TEXT NOT NULL,
+    date_sent TIMESTAMP
+);
+
+
+-- MySQL
+-- CREATE TABLE users (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     full_name VARCHAR(300) NOT NULL,
+--     yearly_salary INT CHECK(yearly_salary > 0) ,
+--     current_status ENUM('employed', 'self-employed', 'unemployed') -- MySQL Syntax
+-- );
+
+-- CREATE TABLE employers(
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     company_name VARCHAR(300) NOT NULL,
+--     company_address VARCHAR(300) NOT NULL,
+--     yearly_revenue FLOAT CHECK (yearly_revenue > 0),
+--     is_hiring BOOLEAN DEFAULT FALSE
+-- );
+
+-- CREATE TABLE conversations (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     user_name VARCHAR(200),
+--     employer_name VARCHAR(250),
+--     message TEXT NOT NULL,
+--     date_sent TIMESTAMP
+-- );
