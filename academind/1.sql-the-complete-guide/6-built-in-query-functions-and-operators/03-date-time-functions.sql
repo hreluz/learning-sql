96b466ef5d663cb23483a36aae7e9be481afb9d8
Hector <hmreluz@gmail.com>
2024-08-08 10:55:31 -0500

    academind: sql-complete-guide: 5-groupping-and-aggregate functions -  weekdays and timestamps

--- academind/1.sql-the-complete-guide/6-built-in-query-functions-and-operators/03-date-time-functions.sql
+++ academind/1.sql-the-complete-guide/6-built-in-query-functions-and-operators/03-date-time-functions.sql
@@ -6,3 +6,20 @@ FROM memberships;
 
 SELECT EXTRACT(MINUTE FROM last_checkin), last_checkin
 FROM memberships;
+
+-- For MySQL
+-- SELECT CONVERT(last_checkin,  DATE), CONVERT(last_checkin,  TIME)
+-- FROM memberships
+
+-- SELECT EXTRACT(DOW FROM last_checkin), last_checkin -- for PostgreSQL
+-- SELECT WEEKDAY(last_checkin) + 1, last_checkin -- for MySQL
+-- FROM memberships; 
+
+
+-- For PostgreSQL
+
+SELECT EXTRACT(DOW FROM last_checkin), last_checkin -- for PostgreSQL
+FROM memberships; 
+
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
+FROM memberships
