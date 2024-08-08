76d6b89fa60d9961fbc9f6375278cbae23f437ce
Hector <hmreluz@gmail.com>
2024-08-07 21:18:12 -0500

    academind: sql-complete-guide: 3-data-relationships-and-joining-data: add first tables and relations

---
+++ academind/1.sql-the-complete-guide/3-data-relationships-and-joining-data/18-insert-company-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, birthdate, email)
+VALUES ('Bruce', 'Wayne', '1990-01-01', 'batman@gmail.com');
+
+INSERT INTO intranet_accounts (email, password)
+VALUES ('batman@gmail.com', '123123');
