36321bc9a6e541065a26e725153ed273003237d0
Hector <hmreluz@gmail.com>
2024-08-07 23:08:04 -0500

    academind: sql-complete-guide: 4-more-on-pks-fk: self reference relationships

---
+++ academind/1.sql-the-complete-guide/4-more-on-primary-foreign-keys/04-insert-self-referencing-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (first_name, last_name, supervisor_id)
+VALUES 
+('Bruce', 'Wayne', NULL),
+('Clark', 'Kent', 1),
+('Flash', 'Gordon', 1);
+
+-- DELETE FROM employees where id = 1;
