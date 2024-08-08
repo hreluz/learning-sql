79488336201ae6ca34823cf6ba654c7513ec516c
Hector <hmreluz@gmail.com>
2024-08-08 09:19:52 -0500

    academind: sql-complete-guide: 5-groupping-and-aggregate functions - nested queries

---
+++ academind/1.sql-the-complete-guide/5-groupping-and-aggregate-functions/07-nestes-subqueries.sql
@@ -0,0 +1,23 @@
+ SELECT booking_date, SUM(amount_billed) AS daily_sum
+FROM bookings
+GROUP BY booking_date;
+
+SELECT MIN(daily_table.daily_sum)
+FROM (
+    SELECT booking_date, SUM(amount_billed) AS daily_sum
+    FROM bookings
+    GROUP BY booking_date
+) AS daily_table;
+
+SELECT booking_date
+FROM bookings
+GROUP BY booking_date
+HAVING SUM(amount_billed) = 
+(
+    SELECT MIN(daily_table.daily_sum)
+    FROM (
+        SELECT booking_date, SUM(amount_billed) AS daily_sum
+        FROM bookings
+        GROUP BY booking_date
+    ) AS daily_table
+)
