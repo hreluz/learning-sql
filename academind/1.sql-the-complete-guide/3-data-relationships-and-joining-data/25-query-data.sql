fb3c148851a22a654976791a39dce13b0436c9eb
Hector <hmreluz@gmail.com>
2024-08-07 22:00:12 -0500

    academind: sql-complete-guide: 3-data-relationships-and-joining-data: joining and filtering

--- academind/1.sql-the-complete-guide/3-data-relationships-and-joining-data/25-query-data.sql
+++ academind/1.sql-the-complete-guide/3-data-relationships-and-joining-data/25-query-data.sql
@@ -1,6 +1,18 @@
 -- Get employees and which projects they are working
 
-SELECT e.id, e.first_name, e.last_name, p.title, p.deadline
-FROM employees e
-LEFT JOIN projects_employees pe ON pe.employee_id = e.id
-LEFT JOIN projects p ON p.id = pe.project_id;
+-- SELECT e.id, e.first_name, e.last_name, p.title, p.deadline
+-- FROM employees e
+-- LEFT JOIN projects_employees pe ON pe.employee_id = e.id
+-- LEFT JOIN projects p ON p.id = pe.project_id;
+
+
+SELECT e.id as employee_id, e.first_name, e.last_name, t.name
+FROM employees AS e
+INNER JOIN teams AS t ON t.id = e.team_id
+WHERE t.id = 2;
+
+SELECT e.id as employee_id, e.first_name, e.last_name, t.name, b.name
+FROM employees AS e
+INNER JOIN teams AS t ON t.id = e.team_id
+INNER JOIN company_buildings AS b ON b.id = t.building_id
+WHERE b.id = 3;
