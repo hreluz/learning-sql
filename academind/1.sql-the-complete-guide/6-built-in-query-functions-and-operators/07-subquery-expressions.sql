5b454a3a7b2f49b6cdca732b13d4f27000b99620
Hector <hmreluz@gmail.com>
2024-08-08 12:23:36 -0500

    academind: sql-complete-guide: 5-groupping-and-aggregate functions -  exists

---
+++ academind/1.sql-the-complete-guide/6-built-in-query-functions-and-operators/07-subquery-expressions.sql
@@ -0,0 +1,11 @@
+-- SELECT first_name, last_name
+-- FROM customers
+-- WHERE email = "max@test.com"
+
+SELECT EXISTS(
+    SELECT first_name, last_name
+FROM customers
+WHERE email = "manu@test.com"
+);
+
+
