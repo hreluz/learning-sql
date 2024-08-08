e36f4cebbbb1aa53dae1d5aec4bcc6aae65c0a94
Hector <hmreluz@gmail.com>
2024-08-08 08:53:48 -0500

    academind: sql-complete-guide: 5-groupping-and-aggregate functions - group by and join queries

--- academind/1.sql-the-complete-guide/5-groupping-and-aggregate-functions/05-group-by.sql
+++ academind/1.sql-the-complete-guide/5-groupping-and-aggregate-functions/05-group-by.sql
@@ -1,6 +1,13 @@
 SELECT booking_date, SUM(num_guests)
 FROM bookings
-GROUP BY booking_date
+GROUP BY booking_date;
 
--- SELECT DISTINCT(booking_date), SUM(num_guests)
--- FROM bookings
+SELECT p.name, SUM(b.num_guests)
+FROM payment_methods AS p
+INNER JOIN bookings AS b ON p.id = b.payment_id
+GROUP BY p.name;
+
+SELECT b.booking_date, SUM(b.num_guests), p.name, round(AVG(b.amount_tipped), 2)
+FROM bookings AS b
+INNER JOIN payment_methods AS p ON b.payment_id = p.id
+GROUP BY p.id, b.booking_date
