027971188553c5229b5a53cd3d6f08b1b7195978
Hector <hmreluz@gmail.com>
2024-08-08 12:27:19 -0500

    academind: sql-complete-guide: 5-groupping-and-aggregate functions -  subquery expressions and exists

--- academind/1.sql-the-complete-guide/6-built-in-query-functions-and-operators/07-subquery-expressions.sql
+++ academind/1.sql-the-complete-guide/6-built-in-query-functions-and-operators/07-subquery-expressions.sql
@@ -2,10 +2,25 @@
 -- FROM customers
 -- WHERE email = "max@test.com"
 
-SELECT EXISTS(
-    SELECT first_name, last_name
-FROM customers
-WHERE email = "manu@test.com"
+-- SELECT EXISTS(
+--     SELECT first_name, last_name
+-- FROM customers
+-- WHERE email = "manu@test.com"
+-- );
+
+
+
+SELECT o.id
+FROM orders AS o
+WHERE EXISTS (
+    SELECT email
+    FROM customers as c
+    WHERE o.customer_id = c.id AND c.email = 'manu@test.com'
 );
-
-
+SELECT o.id
+FROM orders AS o
+WHERE EXISTS (
+    SELECT email
+    FROM customers as c
+    WHERE o.customer_id = c.id AND c.email = 'max@test.com'
+);
