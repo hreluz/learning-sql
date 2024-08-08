37b200c6877b296525a93428c3b29f058de1643e
Hector <hmreluz@gmail.com>
2024-08-08 09:27:30 -0500

    academind: sql-complete-guide: 5-groupping-and-aggregate functions - window function

---
+++ academind/1.sql-the-complete-guide/5-groupping-and-aggregate-functions/08-window-function.sql
@@ -0,0 +1,9 @@
+-- SELECT booking_date, amount_tipped
+-- FROM bookings
+
+SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER()
+FROM bookings;
+
+SELECT booking_date, amount_tipped, SUM(amount_tipped) OVER(PARTITION BY booking_date)
+FROM bookings;
+
