51501a956a4db567f865bdc0873a0e76c36896c2
Hector <hmreluz@gmail.com>
2024-08-15 17:33:18 -0500

    academind: sql-complete-guide: 08-indexes - explaining queries and query planning

--- academind/1.sql-the-complete-guide/08-using-indexes/01-stating-setup.sql
+++ academind/1.sql-the-complete-guide/08-using-indexes/01-stating-setup.sql
@@ -1,8 +1,8 @@
 -- CREATE DATABASE indexes; -- Run this first + set up new connection thereafter
 CREATE TABLE users (
-  -- id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   -- MySQL
-  id SERIAL PRIMARY KEY,
+  -- id SERIAL PRIMARY KEY,
   -- -- Postgresql
   first_name VARCHAR(300) NOT NULL,
   last_name VARCHAR(300) NOT NULL,
@@ -11,8 +11,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE addresses (
-  -- id INT PRIMARY KEY AUTO_INCREMENT,
-  id SERIAL PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  -- id SERIAL PRIMARY KEY,
   street VARCHAR(300) NOT NULL,
   house_number VARCHAR(20) NOT NULL,
   postal_code VARCHAR(10) NOT NULL,
